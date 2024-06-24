fa2f2aa10d2441d7bb05fab86fe6848937f75d14
Felipe De Souza <146121300+FelipeSoouza@users.noreply.github.com>
2024-06-24 16:42:38 -0300

    Add files via upload

---
+++ if ,elsif and else csharp.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace studyCsharp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o hora atual: ");
+
+            int hora = int.Parse(Console.ReadLine());
+
+            if (hora < 12)
+            {
+                Console.WriteLine("Bom dia");
+            }
+
+            else if (hora < 18)
+            {
+                Console.WriteLine("boa tarde");
+            }
+
+            else 
+            {
+                Console.WriteLine("boa noite");
+            }
+        }
+    }
+}
